3f2a9d23dd8897838bff48e0abb01d037cd61981
36 <recao@deloitte.com.cn>
2022-07-27 14:11:55 +0800

    ww

---
+++ src/dawd.java
@@ -0,0 +1,10 @@
+/**
+ * @author
+ * @create 2022-07-27
+ */
+public class dawd {
+    public static void main(String[] args) {
+        int s=19;
+        System.out.println(s);
+    }
+}
---
+++ src/ww.java
@@ -0,0 +1,6 @@
+/**
+ * @author
+ * @create 2022-07-27
+ */
+public class ww {
+}
